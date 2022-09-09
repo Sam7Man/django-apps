62f8aecdd87f338683266189460dbcabbf360b6b
Samuel Halomoan Manalu <samman.r007@gmail.com>
2022-09-09 17:31:17 +0700

    Update settings

--- django-payment/djangostripe/djangostripe/settings.py
+++ django-payment/djangostripe/djangostripe/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'djangostripe.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'djangostripe', 
+        'NAME': 'django-payment', 
         'USER': 'postgres', 
         'PASSWORD': 'root',
         'HOST': '127.0.0.1', 
