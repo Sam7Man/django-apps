b3d8c5ed1055e3795ee328d13cc082c9abee78c3
Samuel Halomoan Manalu <samman.r007@gmail.com>
2022-09-12 13:46:21 +0700

    Initial commit

---
+++ django-weather-app/project/weatherapp/views.py
@@ -0,0 +1,39 @@
+from time import time
+import requests
+import datetime
+import time
+from django.shortcuts import render
+
+
+# Create your views here.
+def index(request):
+
+    if 'city' in request.POST:
+        city = request.POST['city']
+    else:
+        city = 'Jakarta'
+
+    appid = 'b3fbfdc6bd795970734a975b5779173e'
+    URL = 'https://api.openweathermap.org/data/2.5/weather'
+    PARAMS = {'q': city, 'units': 'metric', 'lang': 'id', 'appid': appid}
+
+    req = requests.get(url=URL, params=PARAMS)
+    res = req.json()
+    description = res['weather'][0]['description']
+    main = res['weather'][0]['main']
+    icon = res['weather'][0]['icon']
+    temp = res['main']['temp']
+    day = datetime.date.today().strftime('%A, %d %b %Y')
+    clock = time.strftime('%H.%M')
+
+    return render(
+        request, 'weatherapp/index.html', {
+            'descriptions': description,
+            'main': main,
+            'icon': icon,
+            'temp': temp,
+            'day': day,
+            'clock': clock,
+            'city': city,
+        }
+    )
